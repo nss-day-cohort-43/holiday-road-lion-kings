fbd72ebf900246737bd040929a99b69ae81ccd52
Hanako Hashiguchi <hanako.e.hashiguchi@gmail.com>
2020-09-11 14:43:50 -0500

    for got to save one small bit in main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,5 @@
 import { dropDownMenu, hoverEvent } from "./UserInterface/user.js";
 import { StateSelect } from "./state/StateSelect.js"
-<<<<<<< HEAD
-import { getParks } from "./parks/ParkProvider.js";
-// dropDownMenu();
-
-StateSelect();
-getParks()
-=======
 import { useWeather, getWeather } from "./weather/WeatherProvider.js";
 
 getWeather();
@@ -16,4 +9,3 @@ useWeather();
 dropDownMenu();
 hoverEvent();
 StateSelect();
->>>>>>> master
