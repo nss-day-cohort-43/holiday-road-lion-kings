da9ddb5392e6175e8c1eb9f445693d6351b406ab
Terra Roush <terra.roush@gmail.com>
2020-09-11 14:14:36 -0500

    phase one of HTML for eatery

---
+++ scripts/eateries/EateryHTML.js
@@ -0,0 +1,15 @@
+// I need to add logic to better display amenities. like, "if true add 'yes'"
+
+export const EateryHTML = (eateryObj) => {
+    return `
+        <h3>${eateryObj.businessName}</h3>
+        <p>${eateryObj.description}</>
+        <>${eateryObj.city}, ${eateryObj.state}</>
+
+        <ul>Amenities</ul>
+        <li>Pet Friendly? ${eateryObj.petFriendly}</li>
+        <li>Diaper Facility? ${eateryObj.diaperFacility}</li>
+        <li>Playground? ${eateryObj.playground}</li>
+        <li>Restrooms? ${eateryObj.restrooms}</li>
+    `
+}
