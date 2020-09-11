6d5f91be6b4df348956d739b4d69258a53eb473d
Terra Roush <terra.roush@gmail.com>
2020-09-10 21:36:29 -0500

    finished EateryProvider.js. 90% done with EaterySelect.js-still need to confirm my target container and a button. Will need to test.

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,9 @@
 // import { dropDownMenu } from "./UserInterface/user.js";
 import { StateSelect } from "./state/StateSelect.js"
+import { useWeather, getWeather } from "./weather/WeatherProvider.js";
 // dropDownMenu();
+
+useWeather();
+getWeather();
 
 StateSelect();
--- scripts/weather/WeatherProvider.js
+++ scripts/weather/WeatherProvider.js
@@ -1,4 +1,4 @@
-import {} from "../Settings.js"
+// import weatherKey from "./Settings.js"
 
 // need an event listener to get lat and lon from park
 
@@ -7,11 +7,11 @@ let weather = [];
 export const useWeather = () => weather.slice();
 
 export const getWeather = (lat, lon) => {
-  return fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&
-  exclude={part}&appid=${.weatherKey}`)
+  return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=`)
   .then((response) => response.json()
   .then((parsedWeather) => {
-      weather = parsedWeather
+      weather = parsedWeather.list
+      console.log(weather)
     })
   )
 };
