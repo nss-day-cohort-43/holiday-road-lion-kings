839a488b6f89be27ed9a0d5dcba296a28ed182ad
Tristanwyatt <tristan7306@gmail.com>
2020-09-11 10:22:31 -0500

    menu updated

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,8 @@
-// import { dropDownMenu } from "./UserInterface/user.js";
+import { dropDownMenu, hoverEvent } from "./UserInterface/user.js";
 import { StateSelect } from "./state/StateSelect.js"
-// dropDownMenu();
 
+
+
+dropDownMenu();
+hoverEvent();
 StateSelect();
