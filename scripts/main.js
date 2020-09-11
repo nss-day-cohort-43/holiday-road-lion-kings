b59de1c19b4e002f444ba308074fd974e41adf72
Hanako Hashiguchi <hanako.e.hashiguchi@gmail.com>
2020-09-11 16:02:41 -0500

    finished park provider and got the correct parks to pull in

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 import { dropDownMenu, hoverEvent } from "./UserInterface/user.js";
 import { StateSelect } from "./state/StateSelect.js"
 import { EaterySelect } from "./eateries/EaterySelect.js";
+import { getParks } from "./parks/ParkProvider.js";
 
 
 
@@ -10,3 +11,4 @@ dropDownMenu();
 hoverEvent();
 StateSelect();
 EaterySelect();
+getParks();
--- scripts/parks/ParkProvider.js
+++ scripts/parks/ParkProvider.js
@@ -6,8 +6,12 @@ const keys = () => {
 
 const eventHub = document.querySelector(".container");
 
-eventHub.addEventListener("stateChosen", e => {
+let chosenStateCode = ""
 
+eventHub.addEventListener("stateChosen", e => {
+    debugger;
+    chosenStateCode = e.detail.stateChosen
+    getParks(chosenStateCode)
 })
 
 let parks = []
@@ -15,8 +19,8 @@ let parks = []
 export const useParks = () => parks.slice()
 
 
-export const getParks = () => {
-    return fetch(`https://developer.nps.gov/api/v1/parks?stateCode=CO&limit=20&start=0&api_key=${keys()}`)
+export const getParks = (chosenStateCode) => {
+    return fetch(`https://developer.nps.gov/api/v1/parks?stateCode=${chosenStateCode}&limit=20&start=0&api_key=${keys()}`)
         .then(response => response.json()) 
         .then(
             parsedParks => {
