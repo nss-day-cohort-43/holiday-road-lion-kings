83234c1715b10f5f1a9d3c63a5c2256fd9f40333
ste163 <samtedwards@pm.me>
2020-09-14 15:16:09 -0400

    weather module ready for exporting. Needs mapping through the API

--- scripts/weather/WeatherHTML.js
+++ scripts/weather/WeatherHTML.js
@@ -1,34 +1,38 @@
+// Module to generate HTML for weather card
+
+import { } from "./WeatherCard.js"
+
 export const WeatherHTML = (weatherObj) => {
     return `
         <div class="weather__day">
             <h2 class="weather__h2">${weatherObj}</h2>
             <p class="weather__icon">${weatherObj}</p>
             <p class="weather__degrees">${weatherObj} &#176</p>
-            <p class="weather__text">Sun${weatherObj}ny</p>
+            <p class="weather__description">Sun${weatherObj}ny</p>
         </div>
         <div class="weather__day">
             <h2 class="weather__h2">${weatherObj}</h2>
             <p class="weather__icon">${weatherObj}</p>
             <p class="weather__degrees">${weatherObj} &#176</p>
-            <p class="weather__text">${weatherObj}</p>
+            <p class="weather__description">${weatherObj}</p>
         </div>
         <div class="weather__day">
             <h2 class="weather__h2">${weatherObj}</h2>
             <p class="weather__icon">${weatherObj}</p>
             <p class="weather__degrees">${weatherObj} &#176</p>
-            <p class="weather__text">${weatherObj}</p>
+            <p class="weather__description">${weatherObj}</p>
         </div>
         <div class="weather__day">
             <h2 class="weather__h2">${weatherObj}</h2>
             <p class="weather__icon">${weatherObj}</p>
             <p class="weather__degrees">${weatherObj} &#176</p>
-            <p class="weather__text">${weatherObj}</p>
+            <p class="weather__description">${weatherObj}</p>
         </div>
         <div class="weather__day">
             <h2 class="weather__h2">${weatherObj}</h2>
             <p class="weather__icon">${weatherObj}</p>
             <p class="weather__degrees">${weatherObj} &#176</p>
-            <p class="weather__text">${weatherObj}</p>
+            <p class="weather__description">${weatherObj}</p>
         </div>
     `
 }
