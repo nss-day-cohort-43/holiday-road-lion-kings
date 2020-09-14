a9b20b223d06bcc1f4a41200678c5642019f692e
Tristanwyatt <tristan7306@gmail.com>
2020-09-14 14:35:09 -0500

    weatherapi

--- scripts/weather/WeatherProvider.js
+++ scripts/weather/WeatherProvider.js
@@ -10,7 +10,7 @@ let weather = [];
 export const useWeather = () => weather.slice();
 
 export const getWeather = (lat, lon) => {
-  return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=35&lon=139&appid=8c4933087c84341f4eb847eb6ea3b7f6`)
+  return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${keys()}`)
   .then((response) => response.json()
   .then((parsedWeather) => {
       weather = parsedWeather.list
